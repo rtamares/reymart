7c0caeaf5277cdcd1c8bef9fde6f6e1b007c5f37
rtamares <31788567+rtamares@users.noreply.github.com>
2017-09-23 15:54:56 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic Login using PHP and MySql</title>
+</head>
+<body>
+<center><br><br><br><br><br><br>
+<body background="image.png">
+	
+	<form method="post" action="login.php"> 
+		<input type="text" name="txtuname" />
+		<input type="password" name="txtpword"><br><br>
+		<input type="submit" name="login" value="login" style="height: 30px; width: 220px;">
+	</form>
+	<form method="post" action="signup.php"> <br>
+		<input type="text" name="txtuname1" />
+		<input type="password" name="txtpword1"><br><br>
+		<input type="submit" name="signup" value="Signup" style="height: 30px; width: 220px;">
+	</form>
+	</center>
+</body>
+</html>
